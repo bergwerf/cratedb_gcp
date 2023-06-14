8e3a7d6f009e401b31470df6e6c35801c8563902
Herman Bergwerf <post@hbergwerf.nl>
2023-06-14 15:18:53 +0200

    Add Service Account authentication to example.

--- example.sql
+++ example.sql
@@ -7,12 +7,20 @@ CREATE TABLE person (
 INSERT INTO person (first_name, last_name, notes)
 VALUES ('Herman', 'Bergwerf', 'Author');
 
-CREATE REPOSITORY awslocal TYPE s3 WITH (
-  endpoint='http://localhost:4566',
-  protocol='http',
-  bucket='crate-bucket',
-  base_path='my_base_path',
-  access_key='my_access_key',
-  secret_key='my_secret_key',
+--CREATE REPOSITORY aws TYPE s3 WITH (
+--  endpoint='http://localhost:4566',
+--  protocol='http',
+--  bucket='crate-bucket',
+--  base_path='my_base_path',
+--  access_key='my_access_key',
+--  secret_key='my_secret_key',
+--  compress=true
+--);
+
+CREATE REPOSITORY gcloud TYPE gcs WITH (
+  bucket='bucket',
+  base_path='repository',
   compress=true
 );
+
+CREATE SNAPSHOT gcloud.snapshot;
--- gcs_demo/src/main/java/nl/hbergwerf/example/Main.java
+++ gcs_demo/src/main/java/nl/hbergwerf/example/Main.java
@@ -1,12 +1,11 @@
 package nl.hbergwerf.example;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.ByteArrayInputStream;
+import java.io.*;
 import java.nio.channels.Channels;
 import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.ReadChannel;
 import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.*;
@@ -18,12 +17,31 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     // Connect to local mock Cloud Storage instance (running in Docker).
-    storage = StorageOptions.newBuilder()
+    /*storage = StorageOptions.newBuilder()
       .setProjectId("hello")
-      .setHost("http://localhost:4443").build().getService();
+      .setHost("http://localhost:4443").build().getService();*/
+
+    // Read parameters.
+    String prop_path = "src/storage.properties";
+    FileInputStream prop_input = new FileInputStream(prop_path);
+    Properties prop = new Properties();
+    prop.load(prop_input);
+    bucketName = prop.getProperty("bucket_name");
+
+    ServiceAccountCredentials credentials =
+            ServiceAccountCredentials.fromStream(new FileInputStream(
+                    prop.getProperty("service_account_key")));
+
+    // Connect to a bucket via a JSON key.
+    StorageOptions.Builder b = StorageOptions.newBuilder();
+    b.setCredentials(credentials);
+    storage = b.build().getService();
 
     // Create a bucket.
-    Bucket bucket = storage.create(BucketInfo.of(bucketName));
+    //Bucket bucket = storage.create(BucketInfo.of(bucketName));
+
+    // Retrieve a bucket.
+    Bucket bucket = storage.get(bucketName);
 
     // Create blob data.
     byte[] data = "Hello, Cloud Storage!".getBytes(StandardCharsets.UTF_8);
@@ -43,7 +61,7 @@ public class Main {
     writeViaStream("world", new ByteArrayInputStream("Streamed text".getBytes()));
     writeViaStream("hello", new ByteArrayInputStream("Does this overwrite?".getBytes()));
 
-    bucket.create("hello", new byte[] {});
+    //bucket.create("hello", new byte[] {});
   }
 
   public static void printExists(String blobName) {
